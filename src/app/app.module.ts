3b577416d0cbaace5b781a98df3179b22fb32daf
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-11-16 14:29:11 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NameCardComponent } from './name-card/name-card.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/name-card/name-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {Card} from '../card';
+
+@Component({
+  selector: 'app-name-card',
+  templateUrl: './name-card.component.html',
+  styleUrls: ['./name-card.component.css']
+})
+export class NameCardComponent implements OnInit {
+
+  @Input()
+  card: Card = {};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
